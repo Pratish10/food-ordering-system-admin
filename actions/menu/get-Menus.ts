30269332ae6dac1e876ce2b49f91e381141b5de3
Pratish Ninawe <pratish.1006@gmail.com>
2024-04-16 20:08:03 +0530

    updated gitignore for debugger

--- actions/menu/get-Menus.ts
+++ actions/menu/get-Menus.ts
@@ -19,6 +19,18 @@ Array<{
 > => {
   try {
     const menus = await db.menu.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        type: true,
+        image: true,
+        category: true,
+        amount: true,
+        createdAt: true,
+        updatedAt: true,
+        isFeatured: true
+      },
       orderBy: {
         updatedAt: 'desc'
       }
--- app/(site)/menus/page.tsx
+++ app/(site)/menus/page.tsx
@@ -6,10 +6,9 @@ import { getMenus } from '@/actions/menu/get-Menus'
 
 const MenuList = async (): Promise<React.JSX.Element> => {
   const menus = await getMenus()
-
   return (
     <div>
-      <h1 className="text-2xl">Menus</h1>ssss
+      <h1 className="text-2xl">Menus</h1>
       <DataTable
         columns={MenuColumn}
         data={menus}
