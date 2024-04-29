24841b3f9043d94ea2d05ec7391f4425475e4040
Pratish Ninawe <pratish.1006@gmail.com>
2024-04-29 13:39:29 +0530

    fix: nextjs opted caching route
    Signed-off-by: Pratish Ninawe <pratish.1006@gmail.com>

--- app/api/getFeaturedMenus/route.ts
+++ app/api/getFeaturedMenus/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET (): Promise<NextResponse> {
   try {
     const featuredMenus = await db.menu.findMany({
