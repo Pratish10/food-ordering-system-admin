603aa0c249b54a5ee4a8a6e95348329b08b3e5f4
Pratish Ninawe <pratish.1006@gmail.com>
2024-05-05 18:24:41 +0530

    removed force dynamic export
    Signed-off-by: Pratish Ninawe <pratish.1006@gmail.com>

--- app/api/getFeaturedMenus/route.ts
+++ app/api/getFeaturedMenus/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
 
-export const dynamic = 'force-dynamic'
-
 export async function GET (): Promise<NextResponse> {
   try {
     const featuredMenus = await db.menu.findMany({
@@ -29,7 +27,11 @@ export async function GET (): Promise<NextResponse> {
       responseData: featuredMenus
     }
 
-    return new NextResponse(JSON.stringify(response), { status: 200 })
+    const headers = {
+      'Cache-Control': 'no-store'
+    }
+
+    return new NextResponse(JSON.stringify(response), { status: 200, headers })
   } catch (error) {
     console.error('GET_MENUS_ERROR:', error)
     return new NextResponse('Internal Server Error', { status: 500 })
